125b2b4998971c69195b0c1dae89b4ac80be4d3a
dhinakaran-Y <dhinakaran.y.dhina14@gmail.com>
2025-12-14 23:58:37 +0530

    home page added

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [tailwindcss()],
+   server:{
+    host:true
+  },
   build: {
     rollupOptions: {
       input: ["index.html", "about.html"],
